5b4f6c4488c17a76dcb5842d0fd02ae54b067a0d
Manel Brull <mbrull88@gmail.com>
2014-05-14 12:08:03 +0200

    añadidas entidades restantes

---
+++ ControlGestionBecarios/src/main/java/modelo/entidades/Becario.java
@@ -0,0 +1,162 @@
+package modelo.entidades;
+
+import java.util.Date;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
+
+import modelo.entidades.practica.Practica;
+
+@Entity(name = "becario")
+public class Becario {
+	@Id @GeneratedValue
+	private int id;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinTable(
+			name="practicaBecario",
+			joinColumns = @JoinColumn(name="idBecario"),
+			inverseJoinColumns = @JoinColumn (name="idPractica")
+	)
+	private Set <Practica> practicas;
+	
+	@Column(nullable = false)
+	private String nombre;
+	private String apellidos;
+	@Column(nullable = false, unique = true)
+	private String documentacion;
+	private String direccion;
+	private String localidad;
+	private String provincia;
+	private String telefono;
+	private String email;
+	private Boolean darleAltaEnSeguridadSocial;
+	private String numAfiliacionSeguridadSocial;
+	private String cuentaBancaria;
+	private Date fechaCreacion;
+	
+	public Becario(){}
+	
+	
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Set<Practica> getPracticas() {
+		return practicas;
+	}
+
+	public void setPracticas(Set<Practica> practicas) {
+		this.practicas = practicas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDocumentacion() {
+		return documentacion;
+	}
+
+	public void setDocumentacion(String documentacion) {
+		this.documentacion = documentacion;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getLocalidad() {
+		return localidad;
+	}
+
+	public void setLocalidad(String localidad) {
+		this.localidad = localidad;
+	}
+
+	public String getProvincia() {
+		return provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		this.provincia = provincia;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Boolean getDarleAltaEnSeguridadSocial() {
+		return darleAltaEnSeguridadSocial;
+	}
+
+	public void setDarleAltaEnSeguridadSocial(Boolean darleAltaEnSeguridadSocial) {
+		this.darleAltaEnSeguridadSocial = darleAltaEnSeguridadSocial;
+	}
+
+	public String getNumAfiliacionSeguridadSocial() {
+		return numAfiliacionSeguridadSocial;
+	}
+
+	public void setNumAfiliacionSeguridadSocial(String numAfiliacionSeguridadSocial) {
+		this.numAfiliacionSeguridadSocial = numAfiliacionSeguridadSocial;
+	}
+
+	public String getCuentaBancaria() {
+		return cuentaBancaria;
+	}
+
+	public void setCuentaBancaria(String cuentaBancaria) {
+		this.cuentaBancaria = cuentaBancaria;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	};
+
+}
--- ControlGestionBecarios/src/main/java/modelo/entidades/practica/Practica.java
+++ ControlGestionBecarios/src/main/java/modelo/entidades/practica/Practica.java
@@ -3,49 +3,196 @@ package modelo.entidades.practica;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 
+import modelo.entidades.Departamento;
 import modelo.entidades.Expediente;
 import modelo.entidades.TutorAcademico;
 
 @Entity(name = "practica")
 public class Practica {
+	
 	@Id @GeneratedValue
 	private int id;
-	
 	@OneToOne 
 	@JoinColumn(name = "idExpediente", nullable = false)
 	private Expediente expediente;
 	@OneToOne 
 	@JoinColumn(name = "idTutorAcademico")
 	private TutorAcademico tutorAcademico;
-	
-	private int departamentoDestino;
-	
+	@OneToOne 
+	@JoinColumn(name = "idDepartamentoDestino")
+	private Departamento departamentoDestino;
+	@Enumerated(EnumType.STRING)
+	private TipoPractica profesionalesFormacion;
 	private Date fechaInicio;
 	private Date fechaFinalTeorica;
 	private Date fechaFinalReal;
 	private Date fechaCreacion;
-	
 	private String estudiosCursados;
-	
-	private String tipoNumeroDeHoras;
+	@Enumerated(EnumType.STRING)
+	private TipoHoras tipoNumeroDeHoras;
 	private int numHoras;
-	
-	private String tipoBolsaEstudios;
-	private String bolsaDeEstudios;
-	
-	
+	@Enumerated(EnumType.STRING)
+	private TipoBolsaEstudios tipoBolsaEstudios;
+	private int bolsaDeEstudios;
 	private String tutorAyuntamiento;
 	private String decretoAutorizacion;
-	
-	
 	private String refClica;
 	private String observaciones;
 	
+	
+	public Practica(){}
+
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Expediente getExpediente() {
+		return expediente;
+	}
+
+	public void setExpediente(Expediente expediente) {
+		this.expediente = expediente;
+	}
+
+	public TutorAcademico getTutorAcademico() {
+		return tutorAcademico;
+	}
+
+	public void setTutorAcademico(TutorAcademico tutorAcademico) {
+		this.tutorAcademico = tutorAcademico;
+	}
+
+	public Departamento getDepartamentoDestino() {
+		return departamentoDestino;
+	}
+
+	public void setDepartamentoDestino(Departamento departamentoDestino) {
+		this.departamentoDestino = departamentoDestino;
+	}
+
+	public TipoPractica getProfesionalesFormacion() {
+		return profesionalesFormacion;
+	}
+
+	public void setProfesionalesFormacion(TipoPractica profesionalesFormacion) {
+		this.profesionalesFormacion = profesionalesFormacion;
+	}
+
+	public Date getFechaInicio() {
+		return fechaInicio;
+	}
+
+	public void setFechaInicio(Date fechaInicio) {
+		this.fechaInicio = fechaInicio;
+	}
+
+	public Date getFechaFinalTeorica() {
+		return fechaFinalTeorica;
+	}
+
+	public void setFechaFinalTeorica(Date fechaFinalTeorica) {
+		this.fechaFinalTeorica = fechaFinalTeorica;
+	}
+
+	public Date getFechaFinalReal() {
+		return fechaFinalReal;
+	}
+
+	public void setFechaFinalReal(Date fechaFinalReal) {
+		this.fechaFinalReal = fechaFinalReal;
+	}
+
+	public Date getFechaCreacion() {
+		return fechaCreacion;
+	}
+
+	public void setFechaCreacion(Date fechaCreacion) {
+		this.fechaCreacion = fechaCreacion;
+	}
+
+	public String getEstudiosCursados() {
+		return estudiosCursados;
+	}
+
+	public void setEstudiosCursados(String estudiosCursados) {
+		this.estudiosCursados = estudiosCursados;
+	}
+
+	public TipoHoras getTipoNumeroDeHoras() {
+		return tipoNumeroDeHoras;
+	}
+
+	public void setTipoNumeroDeHoras(TipoHoras tipoNumeroDeHoras) {
+		this.tipoNumeroDeHoras = tipoNumeroDeHoras;
+	}
+
+	public int getNumHoras() {
+		return numHoras;
+	}
+
+	public void setNumHoras(int numHoras) {
+		this.numHoras = numHoras;
+	}
+
+	public TipoBolsaEstudios getTipoBolsaEstudios() {
+		return tipoBolsaEstudios;
+	}
+
+	public void setTipoBolsaEstudios(TipoBolsaEstudios tipoBolsaEstudios) {
+		this.tipoBolsaEstudios = tipoBolsaEstudios;
+	}
+
+	public int getBolsaDeEstudios() {
+		return bolsaDeEstudios;
+	}
+
+	public void setBolsaDeEstudios(int bolsaDeEstudios) {
+		this.bolsaDeEstudios = bolsaDeEstudios;
+	}
+
+	public String getTutorAyuntamiento() {
+		return tutorAyuntamiento;
+	}
+
+	public void setTutorAyuntamiento(String tutorAyuntamiento) {
+		this.tutorAyuntamiento = tutorAyuntamiento;
+	}
+
+	public String getDecretoAutorizacion() {
+		return decretoAutorizacion;
+	}
+
+	public void setDecretoAutorizacion(String decretoAutorizacion) {
+		this.decretoAutorizacion = decretoAutorizacion;
+	}
+
+	public String getRefClica() {
+		return refClica;
+	}
+
+	public void setRefClica(String refClica) {
+		this.refClica = refClica;
+	}
+
+	public String getObservaciones() {
+		return observaciones;
+	}
+
+	public void setObservaciones(String observaciones) {
+		this.observaciones = observaciones;
+	};
 	
 	
 	
